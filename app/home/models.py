c026857df3411dbe8a4889ffd4c786311929c8dd
egrisel <etienne.grisel@gmail.com>
2021-03-06 17:43:31 +0100

    Ajouté le modèle de page classique

--- app/home/models.py
+++ app/home/models.py
@@ -22,6 +22,26 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         StreamFieldPanel('body', classname="full"),
     ]
+    
+    max_count = 1
 
     class Meta:
         verbose_name = "page d'accueil"
+
+
+class ClassicPage(Page):
+    body = StreamField([
+        ('paragraph', blocks.RichTextBlock(
+            label='Paragraphe', features=['h1', 'h2', 'h3', 'bold', 'italic', 'link', 'hr']
+        )),
+        ('image', ImageChooserBlock()),
+        ('two_columns', TwoColumnBlock()),
+        ('three_columns', ThreeColumnBlock()),
+    ], verbose_name='Contenu')
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body', classname="full"),
+    ]
+
+    class Meta:
+        verbose_name = "Page classique"
